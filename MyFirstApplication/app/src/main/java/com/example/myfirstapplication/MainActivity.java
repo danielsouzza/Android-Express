35d72afbb1dfaba08830c22cbdb564318f953c89
danielsouzza <daniel.lima.souzza@gmail.com>
2022-07-22 22:13:59 -0300

    My First application

--- MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ MyFirstApplication/app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -1,6 +1,9 @@
- package com.example.myfirstapplication;
+package com.example.myfirstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,7 +15,9 @@ import android.widget.Toast;
 import java.util.HashSet;
 import java.util.Random;
 
- public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,44 +28,58 @@ import java.util.Random;
         TextView txtResult = findViewById(R.id.textResult);
         Button btnGenerate = findViewById(R.id.btn_generate);
 
+        // Banco de dados
+        prefs = getSharedPreferences("db", Context.MODE_PRIVATE);
+        String lastResult = prefs.getString("lastResult",null);
+        if(lastResult != null){
+            txtResult.setText("Ultima aposta: " + lastResult);
+        }
+
         btnGenerate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String text = editText.getText().toString();
-                numberGenerator(text,txtResult);
+                numberGenerator(text, txtResult);
             }
         });
 
     }
 
-    private void numberGenerator(String text, TextView txtResult){
-        if(!text.isEmpty()){
-            int qtd = Integer.parseInt(text);
-
-            // Validar o campo
-            if(qtd >= 6 && qtd <= 15){
-
-                HashSet<Integer> numbers = new HashSet<Integer>();
-                Random random = new Random();
-
-                while (!(numbers.size() == qtd)){
-                    int number = random.nextInt(60);
-                    numbers.add(number + 1);
-                }
-
-                String txt =
-                        numbers.toString()
-                                .replace(",","-")
-                                .replace("[","")
-                                .replace("]","");
-
-                txtResult.setText(txt);
-
-            }else{
-                Toast.makeText(this,"Informe um número entre 6 e 15", Toast.LENGTH_LONG).show();
-            }
-        }else{
-            Toast.makeText(this,"Informe um número entre 6 e 15", Toast.LENGTH_LONG).show();
+    private void numberGenerator(String text, TextView txtResult) {
+        // Vadidar conteúdo
+        if (text.isEmpty()) {
+            Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show();
+            return;
         }
+
+        int qtd = Integer.parseInt(text);
+
+        if (qtd < 6 || qtd > 15) {
+            Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+        HashSet<Integer> numbers = new HashSet<Integer>();
+        Random random = new Random();
+
+        while (!(numbers.size() == qtd)) {
+            int number = random.nextInt(60);
+            numbers.add(number + 1);
+        }
+
+        String txt =
+                numbers.toString()
+                        .replace(",", "-")
+                        .replace("[", "")
+                        .replace("]", "");
+
+        txtResult.setText(txt);
+
+        SharedPreferences.Editor editor =
+                 prefs.edit()
+                .putString("lastResult",txtResult.getText().toString());
+        editor.apply(); // assicrona
+
     }
 }
